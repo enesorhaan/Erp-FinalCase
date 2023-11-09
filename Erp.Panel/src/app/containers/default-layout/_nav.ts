c327407be771d80b79c74f8c52950b1f8f616bb5
enesorhaan <enesorhan_1366@hotmail.com>
2023-11-09 15:50:47 +0300

    bug fixed

--- Erp.Panel/src/app/containers/default-layout/_nav.ts
+++ Erp.Panel/src/app/containers/default-layout/_nav.ts
@@ -4,11 +4,7 @@ export const navItems: INavData[] = [
   {
     name: 'Dashboard',
     url: '/dashboard',
-    iconComponent: { name: 'cil-speedometer' },
-    badge: {
-      color: 'info',
-      text: 'NEW'
-    }
+    iconComponent: { name: 'cil-home' }
   },
   {
     name: 'Dealers',
@@ -149,21 +145,6 @@ export const navItems: INavData[] = [
         url: '/report/list/product'
       }
     ]
-  },
-  {
-    title: true,
-    name: 'Theme'
-  },
-  {
-    name: 'Colors',
-    url: '/theme/colors',
-    iconComponent: { name: 'cil-drop' }
-  },
-  {
-    name: 'Typography',
-    url: '/theme/typography',
-    linkProps: { fragment: 'someAnchor' },
-    iconComponent: { name: 'cil-pencil' }
   },
   {
     name: 'Components',
@@ -353,50 +334,5 @@ export const navItems: INavData[] = [
       color: 'info',
       text: 'NEW'
     }
-  },
-  {
-    title: true,
-    name: 'Extras'
-  },
-  {
-    name: 'Pages',
-    url: '/login',
-    iconComponent: { name: 'cil-star' },
-    children: [
-      {
-        name: 'Login',
-        url: '/login'
-      },
-      {
-        name: 'Register',
-        url: '/register'
-      },
-      {
-        name: 'Error 404',
-        url: '/404'
-      },
-      {
-        name: 'Error 500',
-        url: '/500'
-      }
-    ]
-  },
-  {
-    title: true,
-    name: 'Links',
-    class: 'py-0'
-  },
-  {
-    name: 'Docs',
-    url: 'https://coreui.io/angular/docs/templates/installation',
-    iconComponent: { name: 'cil-description' },
-    attributes: { target: '_blank', class: '-text-dark' },
-    class: 'mt-auto'
-  },
-  {
-    name: 'Try CoreUI PRO',
-    url: 'https://coreui.io/product/angular-dashboard-template/',
-    iconComponent: { name: 'cil-layers' },
-    attributes: { target: '_blank' }
   }
 ];
--- Erp.Panel/src/app/views/pages/pages.module.ts
+++ Erp.Panel/src/app/views/pages/pages.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { Page404Component } from './page404/page404.component';
-import { Page500Component } from './page500/page500.component';
 import { ButtonModule, CardModule, FormModule, GridModule, ListGroupModule, SharedModule } from '@coreui/angular';
 import { IconModule } from '@coreui/icons-angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,9 +12,6 @@ import { AuthService } from 'src/app/services/auth.service';
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent,
-    Page404Component,
-    Page500Component
   ],
   imports: [
     CommonModule,
